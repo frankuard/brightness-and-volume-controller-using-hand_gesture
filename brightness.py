06f57d09a09349b09c4261ec5159681b3011360f
Roshan Karki <roshankarki4956@gmail.com>
2026-06-03 17:44:50 +0545

    Adding the requeired landmarker settings

--- brightness.py
+++ brightness.py
@@ -10,3 +10,15 @@ from mediapipe.tasks.python.vision.core.vision_task_running_mode import VisionTa
 ## ADDING REQUIRED MODEL FOR THIS PROJECT
 
 base_options = python.BaseOptions(model_asset_path="models/hand_landmarker.tasks")
+
+
+latest_result = [None]
+
+def on_result(result,output_image,timestamps_ms):
+    latest_result = result[0]
+    
+options = HandLandmarkerOptions(base_options=base_options,running_mode=VisionTaskRunningMode.LIVE_STREAM,num_hands=2,min_hand_detection_confidence = 0.5, result_callback= on_result)
+
+landmarker = HandLandmarker.create_from_options(options)
+print("HandLandMaker created successfully")
+
