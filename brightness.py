1d23c29f6bb6d035a2e006ee6f4acb2238c798ed
Roshan Karki <roshankarki4956@gmail.com>
2026-06-03 17:41:34 +0545

    Adding required repositories for the brightness.py

--- brightness.py
+++ brightness.py
@@ -5,3 +5,8 @@ import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python.vision import HandLandmarker, HandLandmarkerOptions
 from mediapipe.tasks.python.vision.core.vision_task_running_mode import VisionTaskRunningMode as VisionTaskRunningMode 
+
+
+## ADDING REQUIRED MODEL FOR THIS PROJECT
+
+base_options = python.BaseOptions(model_asset_path="models/hand_landmarker.tasks")
