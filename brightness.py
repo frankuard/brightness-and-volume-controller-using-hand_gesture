d795acfd070300d5f126c7b9120f7ae12db2d4c1
Roshan Karki <roshankarki4956@gmail.com>
2026-06-03 17:39:48 +0545

    Adding required repositories for the brightness.py

---
+++ brightness.py
@@ -0,0 +1,7 @@
+import cv2 
+import time
+import math
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python.vision import HandLandmarker, HandLandmarkerOptions
+from mediapipe.tasks.python.vision.core.vision_task_running_mode import VisionTaskRunningMode as VisionTaskRunningMode 
