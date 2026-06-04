d1c6d3bf7d5b1f8f1f3b96374c7aad4f8e6f62d3
Roshan Karki <roshankarki4956@gmail.com>
2026-06-04 12:17:52 +0545

    Importing required libraries for the volume.py

---
+++ volume.py
@@ -0,0 +1,9 @@
+import cv2
+import time
+import math
+import numpy as np
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python.vision import HandLandmarker,HandLandmarkerOptions 
+from mediapipe.tasks.python.vision.core.vision_task_running_mode import VisionTaskRunningMode as VisionTaskRunningMode
+ 
