cae4ff0d866427c03c71cd48c3678f3ce8d60d65
Roshan Karki <roshankarki4956@gmail.com>
2026-06-04 12:06:26 +0545

    Finalizing the components now only the windows applicable is left for now

--- brightness.py
+++ brightness.py
@@ -1,6 +1,7 @@
 import cv2 
 import time
 import math
+import numpy as np
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python.vision import HandLandmarker, HandLandmarkerOptions
@@ -9,13 +10,13 @@ from mediapipe.tasks.python.vision.core.vision_task_running_mode import VisionTa
 
 ## ADDING REQUIRED MODEL FOR THIS PROJECT
 
-base_options = python.BaseOptions(model_asset_path="models/hand_landmarker.tasks")
+base_options = python.BaseOptions(model_asset_path="models/hand_landmarker.task")
 
 
 latest_result = [None]
 
-def on_result(result,output_image,timestamps_ms):
-    latest_result = result[0]
+def on_result(result,output_image,timestamp_ms):
+    latest_result[0] = result
     
 options = HandLandmarkerOptions(base_options=base_options,running_mode=VisionTaskRunningMode.LIVE_STREAM,num_hands=2,min_hand_detection_confidence = 0.5, result_callback= on_result)
 
@@ -47,7 +48,8 @@ while True:
     
     result = latest_result[0]
     
-    if result and result.hand_landmarkers:
+    if result and result.hand_landmarks:
+                
         
         for i, hand_lms in enumerate(result.hand_landmarks):
             
@@ -61,14 +63,31 @@ while True:
                 
                 cv2.line(frame,(x1,y1),(x2,y2),(255,255,255),2)
                 
-                for j in range(21):
-                    lm =hand_lms[j]
-                    x = int(lm.x*w)
-                    y = int(lm.y*h)
+            for j in range(21):
+                lm =hand_lms[j]
+                x = int(lm.x*w)
+                y = int(lm.y*h)
                     
-                    cv2.circle(frame,(x,y),3,(0,200,0),-1)
+                cv2.circle(frame,(x,y),3,(0,200,0),-1)
                     
-                    cv2.putText(frame,str(j),(x+5,y-5),cv2.FONT_HERSHEY_SIMPLEX,0.3,(255,255,255),1)
+                cv2.putText(frame,str(j),(x+5,y-5),cv2.FONT_HERSHEY_SIMPLEX,0.3,(255,255,255),1)
+            
+            thumb_tip = hand_lms[4]
+            index_tip = hand_lms[8]
+            
+            thumb_x, thumb_y = int(thumb_tip.x*w), int(thumb_tip.y*h)
+            
+            index_x, index_y = int(index_tip.x*w), int(index_tip.y*h)
+            
+            distance = math.hypot(index_x - thumb_x, index_y - thumb_y)
+            
+            min_distance = 20
+            max_distance = 200
+            
+            brightness = int(np.interp(distance, [min_distance,max_distance],[0,100]))
+            
+            
+            cv2.putText(frame, str(int(brightness)),(50,80),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,255),2)
                     
     cv2.imshow("Brightness Controller", frame)
     
