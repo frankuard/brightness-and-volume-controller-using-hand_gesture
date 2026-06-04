c2cf133b37e1fa796e94cb63d4b575d5b487a875
Roshan Karki <roshankarki4956@gmail.com>
2026-06-04 12:21:00 +0545

    Adding model and hand landmarker

--- volume.py
+++ volume.py
@@ -7,3 +7,15 @@ from mediapipe.tasks import python
 from mediapipe.tasks.python.vision import HandLandmarker,HandLandmarkerOptions 
 from mediapipe.tasks.python.vision.core.vision_task_running_mode import VisionTaskRunningMode as VisionTaskRunningMode
  
+ 
+base_options = python.BaseOptions(model_asset_path="models/hand_landmarker.task")
+
+latest_result= [None]
+
+def on_result(result,output_image,timestamp_ms):
+    latest_result[0] = result
+    
+options = HandLandmarkerOptions(base_options=base_options,running_mode = VisionTaskRunningMode.LIVE_STREAM, num_hands = 2, min_hand_detection_confidence = 0.5, result_callback = on_result)
+
+landmarker = HandLandmarker.create_from_options(options)
+print("HandLandMaker createdd successfully!!")
